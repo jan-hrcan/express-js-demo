7656cb8166c0e61546e1c23467e4fbfca28796b5
unknown <hrcan.j.j@gmail.com>
2020-09-12 17:44:40 +0200

    Create simple chat app.

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
 
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+var messages = [
+    {
+        name: 'Jimmy',
+        message: 'Hello!'
+    },
+    {
+        name: 'Johnny',
+        message: 'Hello to you too!'
+    }
+]
+
+app.get('/messages', (req, res) => {
+    res.send(messages)
+})
+
+app.post('/messages', (req, res) => {
+    messages.push(req.body)
+    res.sendStatus(200)
+})
 
 var server = app.listen(3000, () => {
     console.log('Server running on port', server.address().port)
